199a6a452c388b1594a5c124d944fecea5fa5775
Tony Wang <wwwjfy@gmail.com>
2013-03-25 22:56:47 +0800

    use news.ycombinator.com

--- scrape_api.rb
+++ scrape_api.rb
@@ -1,75 +1,59 @@
 # scrape new stories from the unofficial hacker news api
 # the API is broken half the time, so this needs to be made more robust
 
+require 'nokogiri'
 require 'open-uri'
 require 'json'
 require_relative "story"
 require_relative "utils"
 require_relative "model"
 
-base_url = "http://news.ycombinator.com/newest"
-
 puts Time.now
 
-def scrape(max_pages = 1, base_url)
+def scrape()
   new_stories = []
-  url = base_url
   found_known_story = false
   model = Model.load
 
-
-  (1..max_pages).each do |i|
-
-    puts "opening page #{i}: #{url}"
-    
-    r = open(url).readline
-    
-    puts "found #{r.size} characters"
-    
-    doc = JSON.parse r
-
-    nextId = doc["nextId"]
-
-    doc["items"].each do |item|
-
-      itemid = item["id"]
-
-      if Story.where(:hnid => itemid).count > 0
-        found_known_story = true
-        puts "known story: #{itemid}"
-      else      
-        story = Story.new
-        story.hnid = itemid
-        story.link_url = item["url"]
-        story.link_title = item["title"]
-        story.domain = domain(item["url"])
-        story.scraped_at = Time.now
-        story.user = item["postedBy"]
-        story.prediction = model.classify(story) if model
-        new_stories << story
-      end
+  doc = Nokogiri::HTML(open("https://news.ycombinator.com/newest"))
+  doc.xpath("//td[@class='title']/a").each do |node|
+    url = node.attr("href")
+    if /^item/.match(url)
+        url = "http://news.ycombinator.com/" + url
     end
-    
-    break if found_known_story
-    break unless nextId
+    if /^\/x/.match(url)
+        next
+    end
+    title = node.text
+    subtext = node.parent.parent.next.xpath(".//td[@class='subtext']")
+    # score = subtext.xpath(".//span").text
+    user = subtext.xpath(".//a").first.text
+    id = subtext.xpath(".//a").last.attr("href").sub(/item\?id=/, '').to_i
+    # time = subtext.children[3].text.sub(/^ +(.*) +\| +/, '\1')
 
-    url = "#{base_url}/#{nextId}"
-    puts "moving ahead to #{url}"
+    if Story.where(:hnid => id).count > 0
+      found_known_story = true
+      puts "known story: #{id}"
+    else
+      story = Story.new
+      story.hnid = id
+      story.link_url = url
+      story.link_title = title
+      story.domain = domain(url)
+      story.scraped_at = Time.now
+      story.user = user
+      story.prediction = model.classify(story) if model
+      new_stories << story
+    end
+
+    break if found_known_story
   end
-  
+
   puts "found #{new_stories.size} new stories"
   new_stories.each do |s|
     s.save
-    puts "new story:"
-    puts s.hnid
-    puts s.link_title
-    puts s.link_url
   end
 end
 
 
-begin
-  scrape(20,"http://api.ihackernews.com/new")
-rescue
-  puts "new failed, try again later"
-end
+scrape()
